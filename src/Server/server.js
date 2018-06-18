0d2baed34f3628a9aa5aad097a0e509d6158d685
ShiweiCao <caoshiwei93@gmail.com>
2018-06-18 13:41:37 -0700

    basic function

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,36 +1,97 @@
-const express    = require('express');        
-const bodyParser = require('body-parser');
-const path = require('path');
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-const app = new express();  
-const router = require('./Router/employees')
+let users = new Map();
+users.set("Lobby1", []);
+let rooms = ["Lobby1"];
+let curId = 1;
 
-const mongoose   = require('mongoose');
-mongoose.connect('mongodb://admin:admin@ds014658.mlab.com:14658/mydb');
+io.on("connection", socket => {
+    socket.on("login", res => {
+        users.get("Lobby1").push(`Guest${curId}`);
+        let userName = `Guest${curId++}`;
+        socket.emit("loginSuccess", {
+            userName: userName,
+            rooms: rooms,
+            room: "Lobby1"
+        });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.use((req, res, next) => {
-    console.log(req.method + " " + req.originalUrl);
-    next();
-});
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    socket.join("Lobby1");
+    socket.emit("updateChat", "SERVER", `You have entered Lobby1.`);
+    socket.emit(
+        "updateChat",
+        "SERVER",
+        `Users currently in Lobby1: ${users.get("Lobby1")}`
     );
-    next();
+
+    socket.broadcast
+        .to("Lobby1")
+        .emit("updateChat", "SERVER", `'${userName}' has joined this room.`);
+    });
+
+    socket.on("sendChat", (name, message, room) => {
+        socket.emit("updateChat", "You", message);
+        socket.broadcast.to(room).emit("updateChat", name, message);
+    });
+
+    socket.on("changeName", (oldName, newName, room) => {
+        delete users[oldName];
+        let tmp = users.get(room);
+        let index = tmp.indexOf(oldName);
+        tmp.splice(index, 1, newName);
+        users.set(room, tmp);
+
+    socket.emit(
+        "updateChat",
+        "SERVER",
+        `You have changed your name to '${newName}'`
+    );
+    socket.broadcast
+        .to(room)
+        .emit(
+            "updateChat",
+            "SERVER",
+            `'${oldName}' has changed name to '${newName}'.`
+        );
+    });
+
+    socket.on("changeRoom", (prevRoom, nextRoom, name) => {
+        if (prevRoom === nextRoom) {
+        socket.emit("updateChat", "SERVER", `You\`ve already in ${prevRoom}`);
+        return;
+        }
+        if (!rooms.includes(nextRoom)) {
+        rooms.push(nextRoom);
+        }
+        socket.leave(prevRoom);
+        socket.join(nextRoom);
+        if (!users.has(nextRoom)) {
+        users.set(nextRoom, []);
+        }
+        let array1 = users.get(prevRoom);
+        array1.splice(array1.indexOf(name), 1);
+        users.set(prevRoom, array1);
+        users.set(nextRoom, [...users.get(nextRoom), name]);
+
+        socket.emit("updateChat", "SERVER", `You have entered ${nextRoom}`);
+        socket.emit(
+            "updateChat",
+            "SERVER",
+            `Users currently in ${nextRoom}: ${users.get(nextRoom)}`
+        );
+
+        socket.broadcast
+            .to(prevRoom)
+            .emit("updateChat", "SERVER", `'${name}' has left the room.`);
+        
+        socket.broadcast
+            .to(nextRoom)
+            .emit("updateChat", "SERVER", `'${name}' has joined this room.`);
+        io.local.emit("updateRooms", rooms);
+    });
 });
 
-app.use('/employees', router);
+const port = 8888;
 
-app.listen(8888, () => {
-    console.log('Listening on port 8888')
-});
-
-
+server.listen(port, () => console.log(`Listening on port ${port}`));
